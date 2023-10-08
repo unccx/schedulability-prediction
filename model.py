371190ca92662424ec9550b3ed7e149208fb4824
arc <2336429253@qq.com>
2023-10-08 15:05:59 +0800

    HGNNP添加He初始化，添加模型HyperGCN、HNHN

--- model.py
+++ model.py
@@ -3,6 +3,9 @@ import torch.nn as nn
 
 import dhg
 from dhg.nn import HGNNPConv
+from dhg.nn import HNHNConv
+from dhg.nn import HyperGCNConv
+from dhg.structure.graphs import Graph
 from dhg.nn import UniGCNConv, UniGATConv, UniSAGEConv, UniGINConv, MultiHeadWrapper
 
 class ScorePredictor(nn.Module):
@@ -46,6 +49,14 @@ class HGNNP(nn.Module):
             HGNNPConv(hid_channels, out_channels, use_bn=use_bn, is_last=True)
         )
         self.pred = ScorePredictor(out_channels)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        r"""Initialize learnable parameters.
+        """
+        for layer in self.layers:
+            nn.init.kaiming_uniform_(layer.theta.weight)
+            nn.init.constant_(layer.theta.bias, 0)
 
     def forward(self, X: torch.Tensor, hg: "dhg.Hypergraph") -> torch.Tensor:
         r"""The forward function.
@@ -58,6 +69,103 @@ class HGNNP(nn.Module):
             X = layer(X, hg)
 
         return X
+    
+class HNHN(nn.Module):
+    r"""The HNHN model proposed in `HNHN: Hypergraph Networks with Hyperedge Neurons <https://arxiv.org/pdf/2006.12278.pdf>`_ paper (ICML 2020).
+
+    Args:
+        ``in_channels`` (``int``): :math:`C_{in}` is the number of input channels.
+        ``hid_channels`` (``int``): :math:`C_{hid}` is the number of hidden channels.
+        ``out_channels`` (``int``): :math:`C_{out}` is the number of output channels.
+        ``use_bn`` (``bool``): If set to ``True``, use batch normalization. Defaults to ``False``.
+        ``drop_rate`` (``float``, optional): Dropout ratio. Defaults to ``0.5``.
+    """
+
+    def __init__(
+        self,
+        in_channels: int,
+        hid_channels: int,
+        out_channels: int,
+        use_bn: bool = False,
+        drop_rate: float = 0.5,
+    ) -> None:
+        super().__init__()
+        self.layers = nn.ModuleList()
+        self.layers.append(
+            HNHNConv(in_channels, hid_channels, use_bn=use_bn, drop_rate=drop_rate)
+        )
+        self.layers.append(
+            HNHNConv(hid_channels, out_channels, use_bn=use_bn, is_last=True)
+        )
+
+    def forward(self, X: torch.Tensor, hg: "dhg.Hypergraph") -> torch.Tensor:
+        r"""The forward function.
+
+        Args:
+            ``X`` (``torch.Tensor``): Input vertex feature matrix. Size :math:`(N, C_{in})`.
+            ``hg`` (``dhg.Hypergraph``): The hypergraph structure that contains :math:`N` vertices.
+        """
+        for layer in self.layers:
+            X = layer(X, hg)
+        return X
+    
+class HyperGCN(nn.Module):
+    r"""The HyperGCN model proposed in `HyperGCN: A New Method of Training Graph Convolutional Networks on Hypergraphs <https://papers.nips.cc/paper/2019/file/1efa39bcaec6f3900149160693694536-Paper.pdf>`_ paper (NeurIPS 2019).
+    
+    Args:
+        ``in_channels`` (``int``): :math:`C_{in}` is the number of input channels.
+        ``hid_channels`` (``int``): :math:`C_{hid}` is the number of hidden channels.
+        ``out_channels`` (``int``): :math:`C_{out}` is the number of output channels.
+        ``use_mediator`` (``str``): Whether to use mediator to transform the hyperedges to edges in the graph. Defaults to ``False``.
+        ``fast`` (``bool``): If set to ``True``, the transformed graph structure will be computed once from the input hypergraph and vertex features, and cached for future use. Defaults to ``True``.
+        ``drop_rate`` (``float``, optional): Dropout ratio. Defaults to 0.5.
+    """
+
+    def __init__(
+        self,
+        in_channels: int,
+        hid_channels: int,
+        out_channels: int,
+        use_mediator: bool = False,
+        use_bn: bool = False,
+        fast: bool = True,
+        drop_rate: float = 0.5,
+    ) -> None:
+        super().__init__()
+        self.fast = fast
+        self.cached_g = None
+        self.with_mediator = use_mediator
+        self.layers = nn.ModuleList()
+        self.layers.append(
+            HyperGCNConv(
+                in_channels, hid_channels, use_mediator, use_bn=use_bn, drop_rate=drop_rate,
+            )
+        )
+        self.layers.append(
+            HyperGCNConv(
+                hid_channels, out_channels, use_mediator, use_bn=use_bn, is_last=True
+            )
+        )
+
+    def forward(self, X: torch.Tensor, hg: "dhg.Hypergraph") -> torch.Tensor:
+        r"""The forward function.
+
+        Args:
+            ``X`` (``torch.Tensor``): Input vertex feature matrix. Size :math:`(N, C_{in})`.
+            ``hg`` (``dhg.Hypergraph``): The hypergraph structure that contains :math:`N` vertices.
+        """
+        if self.fast:
+            if self.cached_g is None:
+                self.cached_g = Graph.from_hypergraph_hypergcn(
+                    hg, X, self.with_mediator
+                )
+            for layer in self.layers:
+                X = layer(X, hg, self.cached_g)
+        else:
+            for layer in self.layers:
+                X = layer(X, hg)
+        return X
+
 
 class UniGCN(nn.Module):
     r"""The UniGCN model proposed in `UniGNN: a Unified Framework for Graph and Hypergraph Neural Networks <https://arxiv.org/pdf/2105.00956.pdf>`_ paper (IJCAI 2021).
