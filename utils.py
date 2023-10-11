fb2fa98ff3ef4c68fbf36f849bb8beff39f02abe
arc <2336429253@qq.com>
2023-10-11 13:27:40 +0800

    修改计算利用率函数的api

--- utils.py
+++ utils.py
@@ -1,9 +1,7 @@
-import numpy as np
 from dhg import Hypergraph
 import torch
-import torch.nn as nn
-import matplotlib.pyplot as plt
 import itertools
+import numpy as np
 
 def calculate_sparsity(matrix):
     nonzero_elements = np.count_nonzero(matrix)
@@ -17,22 +15,18 @@ def calculate_sparsity(matrix):
 
 # calculate_sparsity(HG.H.to_dense().cpu().numpy())
 
-def calculate_system_utilization(X, scores, labels, pos_hg:Hypergraph, neg_hg:Hypergraph, platform):
+def calculate_system_utilization(dataset, scores, labels):
     """计算系统利用率"""
-    fastest_processor_speed = platform[0]
-    platform_speed = [processor_speed / fastest_processor_speed for processor_speed in platform] # 处理器速度归一化
-    sum_of_normalized_speed = sum(platform_speed) # S_m
-
     y_pred = torch.where(scores >= scores.mean(), 1, 0)
     prediction_correct = torch.eq(y_pred, labels)
 
-    normalized_utilization = X.index_select(dim=1, index=torch.tensor(3).to(torch.device("cuda")))
-    sum_of_normalized_utilization_pos = torch.sparse.mm(pos_hg.H_T, normalized_utilization)
-    sum_of_normalized_utilization_neg = torch.sparse.mm(neg_hg.H_T, normalized_utilization)
+    normalized_utilization = dataset.X.index_select(dim=1, index=torch.tensor(3).to(torch.device("cuda")))
+    sum_of_normalized_utilization_pos = torch.sparse.mm(dataset.pos_hg.H_T, normalized_utilization)
+    sum_of_normalized_utilization_neg = torch.sparse.mm(dataset.neg_hg.H_T, normalized_utilization)
     sum_of_normalized_utilization = torch.cat((sum_of_normalized_utilization_pos, sum_of_normalized_utilization_neg), dim=0)
-    all_system_utilization = sum_of_normalized_utilization / sum_of_normalized_speed
-    pos_utilization = sum_of_normalized_utilization_pos / sum_of_normalized_speed
-    neg_utilization = sum_of_normalized_utilization_neg / sum_of_normalized_speed
+    all_system_utilization = sum_of_normalized_utilization / dataset.S_m
+    pos_utilization = sum_of_normalized_utilization_pos / dataset.S_m
+    neg_utilization = sum_of_normalized_utilization_neg / dataset.S_m
     correct_utilization = all_system_utilization[prediction_correct]
 
     return all_system_utilization.squeeze(), pos_utilization.squeeze(), neg_utilization.squeeze(), correct_utilization.squeeze()
